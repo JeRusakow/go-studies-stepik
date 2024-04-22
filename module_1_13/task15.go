b867ed9f3f9cf68e0704505b7501cab2cd5588b6
JeRusakov <yegorsr@yandex.ru>
2024-04-22 23:09:49 +0300

    task 15 corrected

--- module_1_13/task15.go
+++ module_1_13/task15.go
@@ -10,7 +10,7 @@ import "fmt"
 func main() {
 	var num, dig, newNum int
 	fmt.Scan(&num, &dig)
-	slice := make([]int, 0, 10)
+	slice := make([]int, 0, 10)https://mr-7.ru/articles/2024/04/18/khokkeinyi-krokodil-pod-oknami-v-kalininskom-raione-zhiteli-i-deputaty-spasaiut-skver-i-derevia
 
 	for ; num > 0; num /= 10 {
 		if num%10 != dig {
