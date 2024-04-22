f62be84e2b39a0431109e27aef1c5fd9a13c40bb
JeRusakov <yegorsr@yandex.ru>
2024-04-22 17:07:46 +0300

    task 10 added

---
+++ module_1_13/task10.go
@@ -0,0 +1,19 @@
+/*
+Найдите самое большее число на отрезке от a до b, кратное 7 .
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	for ; b >= a; b-- {
+		if b%7 == 0 {
+			fmt.Println(b)
+			break
+		}
+	}
+}
